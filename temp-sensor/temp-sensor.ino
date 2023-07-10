5b82ffaa1e3877b15e22cb1cfa920dcf71905ca4
AT-Blaze <antranig.douglas@protonmail.com>
2023-07-10 17:17:10 -0400

    temp sensor

---
+++ temp-sensor/temp-sensor.ino
@@ -0,0 +1,45 @@
+#include "DHT.h"
+#include <EEPROM.h>
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin(); // initialize the sensor
+  int cycles = 0;
+}
+
+void loop() {
+  // wait a few seconds between measurements.
+  delay(2000);
+
+  // read humidity
+  float humi  = dht.readHumidity();
+  // read temperature as Celsius
+  float tempC = dht.readTemperature();
+  // read temperature as Fahrenheit
+  float tempF = dht.readTemperature(true);
+
+  // check if any reads failed
+  if (isnan(humi) || isnan(tempC) || isnan(tempF)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: ");
+    Serial.print(humi);
+    Serial.print("%");
+
+    Serial.print("  |  "); 
+
+    Serial.print("Temperature: ");
+    Serial.print(tempC);
+    Serial.print("°C ~ ");
+    Serial.print(tempF);
+    Serial.println("°F");
+  }
+  cycles++;
+  if cycles == 10 (int i = 0; i < 255; i++) {
+      
+    }
+}
