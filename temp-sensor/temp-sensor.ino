df1bce3b487909cbf566c5fd60f37359158bab98
Antranig Douglas <antranig.douglas@protonmail.com>
2023-07-15 16:51:34 -0400

    final version of temp sensor

--- temp-sensor/temp-sensor.ino
+++ temp-sensor/temp-sensor.ino
@@ -5,7 +5,8 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 int cycles = 0;
-int address = 0;
+int tempAddress = 0;
+int humiAddress = 1;
 
 void setup() {
   Serial.begin(9600);
@@ -23,27 +24,51 @@ void loop() {
   // read temperature as Fahrenheit
   float tempF = dht.readTemperature(true);
 
-  // check if any reads failed
-  if (isnan(humi) || isnan(tempC) || isnan(tempF)) {
-    Serial.println("Failed to read from DHT sensor!");
-  } else {
-    Serial.print("Humidity: ");
-    Serial.print(humi);
-    Serial.print("%");
+  // // check if any reads failed
+  // if (isnan(humi) || isnan(tempC) || isnan(tempF)) {
+  //   Serial.println("Failed to read from DHT sensor!");
+  // } else {
+  //   Serial.print("Humidity: ");
+  //   Serial.print(humi);
+  //   Serial.print("%");
 
-    Serial.print("  |  "); 
+  //   Serial.print("  |  "); 
 
-    Serial.print("Temperature: ");
-    Serial.print(tempC);
-    Serial.print("°C ~ ");
-    Serial.print(tempF);
-    Serial.println("°F");
-  }
-  if (cycles%100 == 0) {
-      EEPROM.write(address, tempF);
-      address++;
-      Serial.print(EEPROM.read(address));
+  //   Serial.print("Temperature: ");
+  //   Serial.print(tempC);
+  //   Serial.print("°C ~ ");
+  //   Serial.print(tempF);
+  //   Serial.println("°F");
+  //}
+  if (cycles%1000 == 0) {
+      EEPROM.write(tempAddress, tempF);
+      EEPROM.write(humiAddress, humi);
+      Serial.print(EEPROM.read(tempAddress));
+      Serial.print(EEPROM.read(humiAddress));
+      tempAddress++;
+      humiAddress++;
     }
   cycles++;
-  Serial.println(cycles);
+  //Serial.println(cycles);
 }
+
+// #include <EEPROM.h>
+// int tempAddress = 0;
+// int humiAddress = 1;
+// int address = 0;
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(9600);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+//   // Serial.println(EEPROM.read(tempAddress));
+//   // Serial.println(EEPROM.read(humiAddress));
+//   // tempAddress++;
+//   // humiAddress++;
+//   Serial.println(EEPROM.read(address));
+//   address++;
+//   delay(2000);
+// }
+
